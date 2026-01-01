194b4366875140b534290df28cb1f4cc4e688051
Vasyl Domanchuk <domanchukits@gmail.com>
2026-01-02 00:37:53 +0100

    implement dynamic func

--- 01_task/find_min_coins.py
+++ 01_task/find_min_coins.py
@@ -1,2 +1,25 @@
 def find_min_coins(sum: int, denomination: list[int]) -> dict[int, int]:
-    pass
+    K = [float('inf')] * (sum + 1)
+    K[0] = 0
+    used_coins = [0] * (sum + 1)
+
+    for i in range(sum+1):
+        for coin in denomination:
+            if coin > i:  # try next coin
+                continue
+
+            if K[i-coin] + 1 < K[i]:
+                K[i] = K[i-coin] + 1
+                used_coins[i] = coin
+
+    if K[sum] == float('inf'):  # unable to provide sum
+        return {}
+
+    res = {}
+    # go backward in used coins to restore the denomination of the coins
+    while (sum):
+        used_coin = used_coins[sum]
+        res[used_coin] = res.get(used_coin, 0) + 1
+        sum -= used_coin
+
+    return res
--- 01_task/main.py
+++ 01_task/main.py
@@ -1,10 +1,13 @@
 from find_coins_greedy import find_coins_greedy
+from find_min_coins import find_min_coins
 
 
 def main():
     denomination = [50, 25, 10, 5, 2, 1]
-    res = find_coins_greedy(113, denomination)
-    print(">>>", res)
+    res1 = find_coins_greedy(113, denomination)
+    print("res1", res1)
+    res2 = find_min_coins(113, denomination)
+    print("res2", res2)
 
 
 if __name__ == '__main__':
