a2224c99a576c8b19ec59f523b59ba1f341a3942
Vasyl Domanchuk <domanchukits@gmail.com>
2026-01-01 23:14:52 +0100

    refactor greedy func

--- 01_task/find_coins_greedy.py
+++ 01_task/find_coins_greedy.py
@@ -3,13 +3,13 @@ def find_coins_greedy(sum: int, denomination: list[int]) -> dict[int, int]:
     res = {}
 
     for coin in denomination:
-        num = 0
-        while (coin <= sum):
-            num += 1
-            sum -= coin
+        num = sum // coin
 
-        if num:  # do not track skipped coins
-            res[coin] = num
+        if not num:
+            continue  # try smaller coin
+
+        res[coin] = num
+        sum %= coin
 
         if not sum:  # early return if all coins are found
             return res
